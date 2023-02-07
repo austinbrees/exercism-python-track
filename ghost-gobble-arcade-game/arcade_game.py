1ad85d6b0fd2a51ef203b385248e06b072f351d2
austinbrees <austinbrees@gmail.com>
2023-02-07 22:24:00 +0100

    excercise completed

--- ghost-gobble-arcade-game/arcade_game.py
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -2,45 +2,43 @@
 
 
 def eat_ghost(power_pellet_active, touching_ghost):
-    """Verify that Pac-Man can eat a ghost if he is empowered by a power pellet.
+    if power_pellet_active == True & touching_ghost == True:
+        return True
+    else:
+        return False
 
-    :param power_pellet_active: bool - does the player have an active power pellet?
-    :param touching_ghost: bool - is the player touching a ghost?
-    :return: bool - can the ghost be eaten?
-    """
-
-    pass
 
 
 def score(touching_power_pellet, touching_dot):
-    """Verify that Pac-Man has scored when a power pellet or dot has been eaten.
-
-    :param touching_power_pellet: bool - is the player touching a power pellet?
-    :param touching_dot: bool - is the player touching a dot?
-    :return: bool - has the player scored or not?
-    """
-
-    pass
+    if touching_power_pellet == True or touching_dot == True:
+        return True
+    else:
+        return False
 
 
 def lose(power_pellet_active, touching_ghost):
-    """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost without his power pellet.
-
-    :param power_pellet_active: bool - does the player have an active power pellet?
-    :param touching_ghost: bool - is the player touching a ghost?
-    :return: bool - has the player lost the game?
-    """
-
-    pass
+    if power_pellet_active != True & touching_ghost != False:
+        return True
+    else:
+        return False
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
-    """Trigger the victory event when all dots have been eaten.
+    if has_eaten_all_dots == True & power_pellet_active == True & touching_ghost == True:
+        return True
+    elif has_eaten_all_dots == True & power_pellet_active == True & touching_ghost == False:
+        return True
+    elif has_eaten_all_dots == True & power_pellet_active == True & touching_ghost != True:
+        return True
+    elif has_eaten_all_dots == True & power_pellet_active == False or True & touching_ghost == False:
+        return True
+    else:
+        return False
+    
+    
+    
 
-    :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
-    :param power_pellet_active: bool - does the player have an active power pellet?
-    :param touching_ghost: bool - is the player touching a ghost?
-    :return: bool - has the player won the game?
-    """
-
-    pass
+print(win(True, True, False))
+print(win(True, False, False))
+    
+# %%
