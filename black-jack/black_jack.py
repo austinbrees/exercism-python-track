b6dbbb7243f0eaf843873964dcd1f6c434882ce9
austinbrees <austinbrees@gmail.com>
2023-02-21 18:13:13 +0100

    black jack complete

--- black-jack/black_jack.py
+++ black-jack/black_jack.py
@@ -5,7 +5,8 @@ How to play blackjack:    https://bicyclecards.com/how-to-play/blackjack/
 """
 #%%
 face_cards = {"J": 10, "Q": 10, "K": 10}
-value_cards = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 1}
+numbered_cards = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,}
+black_jack = 21
 
 def value_of_card(card):
     if card in face_cards:
@@ -15,61 +16,66 @@ def value_of_card(card):
     elif card in numbered_cards:
         return int(card)
 
-
 def higher_card(card_one, card_two):
-    for key in value_cards:
-        if card_one == value_cards:
-            card_value = value_cards.get(key)
-            print(card_value)
-            
-higher_card("5", "6")
+    card_one_val = value_of_card(card_one)
+    card_two_val = value_of_card(card_two)
+    
+    if card_one_val > card_two_val:
+        return card_one
+    elif card_two_val > card_one_val:
+        return card_two
+    else:
+        return card_one, card_two
 
 #%%
+def new_value_of_card(card):
+    if card in face_cards:
+        return 10
+    elif card == "A":
+        return 11
+    elif card in numbered_cards:
+        return int(card)
+    
+    
 def value_of_ace(card_one, card_two):
-    """Calculate the most advantageous value for the ace card.
-
-    :param card_one, card_two: str - card dealt. See below for values.
-    :return: int - either 1 or 11 value of the upcoming ace card.
-
-    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
-    2.  'A' (ace card) = 11 (if already in hand)
-    3.  '2' - '10' = numerical value.
-    """
-
-    pass
-
+    card_one = new_value_of_card(card_one)
+    card_two = new_value_of_card(card_two)
+    
+    if card_one + card_two <= 10:
+        return 11
+    else:
+        return 1
+    
 
 def is_blackjack(card_one, card_two):
-    """Determine if the hand is a 'natural' or 'blackjack'.
-
-    :param card_one, card_two: str - card dealt. See below for values.
-    :return: bool - is the hand is a blackjack (two cards worth 21).
-
-    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
-    2.  'A' (ace card) = 11 (if already in hand)
-    3.  '2' - '10' = numerical value.
-    """
-
-    pass
+   card_one = new_value_of_card(card_one)
+   card_two = new_value_of_card(card_two)
+   
+   if card_one + card_two == 21:
+       return True
+   else:
+       return False
 
 
 def can_split_pairs(card_one, card_two):
-    """Determine if a player can split their hand into two hands.
-
-    :param card_one, card_two: str - cards dealt.
-    :return: bool - can the hand be split into two pairs? (i.e. cards are of the same value).
-    """
-
-    pass
+    card_one = new_value_of_card(card_one)
+    card_two = new_value_of_card(card_two)
+   
+    if card_one == card_two:
+       return True
+    else:
+       return False
 
 
 def can_double_down(card_one, card_two):
-    """Determine if a blackjack player can place a double down bet.
-
-    :param card_one, card_two: str - first and second cards in hand.
-    :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
-    """
-
-    pass
+    yes = [9,10,11]
+    card_one = value_of_card(card_one)
+    card_two = value_of_card(card_two)
+    
+    if card_one + card_two in yes:
+        return True
+    else:
+        return False
+    
 
 # %%
