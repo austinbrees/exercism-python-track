8fbd1c21ffc23b4d499fd5ad26d976b0f80b39ef
austinbrees <austinbrees@gmail.com>
2023-03-06 17:20:29 +0100

    leap

---
+++ leap/leap.py
@@ -0,0 +1,14 @@
+def leap_year(year):
+    leap = False
+    
+    if year % 4 == 0:
+        leap = True
+        if year % 100 == 0:
+            leap = False
+            if year % 400 == 0:
+                leap = True
+            
+    return leap
+
+
+print(leap_year(2000))
