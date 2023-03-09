a6c5239f58257adf690dcea18d56ccae47f58175
austinbrees <austinbrees@gmail.com>
2023-03-09 16:29:01 +0100

    diffe downloaded

---
+++ diffie-hellman/diffie_hellman.py
@@ -0,0 +1,19 @@
+
+
+def private_key(p):
+    token = secret.token_hex(2)
+
+
+def public_key(p, g, private):
+    pass
+
+
+def secret(p, public, private):
+    pass
+
+
+import secrets
+
+token = secrets.token_hex(1)
+print(token)
+
